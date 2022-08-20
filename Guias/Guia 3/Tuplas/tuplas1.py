41c9bf744336cf6523a5cf1510b5cfe426e2e15b
Facundo Barneche <fh.barneche@gmail.com>
2022-08-20 14:52:34 -0300

    se empezo la guia 3 (tuplas, diccionarios y conjuntos) y se reacomodo algunas carpetas

---
+++ Guias/Guia 3/Tuplas/tuplas1.py
@@ -0,0 +1,12 @@
+#1. Crea una tupla con números, pide un numero por teclado e indica cuantas veces se repite.
+
+def main():
+
+    tupla = (1, 2, 2, 3, 3, 1, 4, 5, 2, 3, 6, 5)
+
+    num = int(input('Ingrese un numero: '))
+
+    print('hola')
+
+if __name__ == "__name__":
+    main()
