4b9a3af266dd56aa4ce5d5821581250584350477
Facundo Barneche <fh.barneche@gmail.com>
2022-08-27 16:54:02 -0300

    prueba

---
+++ Guias/Guia 3/matplotlib/matplotlib3.py
@@ -0,0 +1,6 @@
+"""
+3.Los siguientes datos de población y superficie pertenecen a países de América y el Caribe. Genera un gr
+áfico que muestre la relación entre población y superficie. Fuente: https://es.wikipedia.org/wiki/Anexo:Pa%C
+3%ADses_de_Am%C3%A9rica_por_poblaci%C3%B3n
+https://es.wikipedia.org/wiki/Anexo:Pa%C3%ADses_de_Am%C3%A9rica_por_superficie
+"""
